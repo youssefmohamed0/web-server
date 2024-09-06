3eb281305962ebfec020e54aed43979d7eb91ec2
youssef <youssefyoussef.m4@gmail.com>
2024-09-07 01:08:18 +0300

    Update webclient.py

--- client/webclient.py
+++ client/webclient.py
@@ -7,10 +7,6 @@ ip = sk.gethostbyname(sk.gethostname())
 
 port = 9999
 
-sock = sk.socket(sk.AF_INET,sk.SOCK_STREAM)
-
-sock.connect((ip,port))
-# connect is used only by the client
 
 
 def send_request(request_bytes):
@@ -20,11 +16,11 @@ def send_request(request_bytes):
 
     response = b""
 
-    while True:
-        packet = sock.recv(1024)
-        if not packet:
-            break
-        response +=packet
+    print("client reciving now")
+    while b'<end>' not in response:
+        response += sock.recv(1024)
+    print("client recieved")
+
 
     sock.close()
 
@@ -41,34 +37,35 @@ Host: {ip}\r\n
 Content-Length: {len(file_bytes)}\r\n
 Content-Type: application/socket-stream\r\n
 Content-Disposition: attachment; filename=\"{filename}\"\r\n
-\r\n
+<end>
 """
     request_bytes = request.encode() + file_bytes
 
+    print("attempting to recieve response from server")
     host_response = send_request(request_bytes)
 
-    headers, body = host_response.split(b'\r\n\r\n', 1)
+    headers, body = host_response.split(b'<end>', 1)
 
     if b"200 OK" in headers:
-        print('File downloaded successfully.')
+        print('File uploaded successfully.')
     else:
-        print('Failed to download file.')
+        print('Failed to upload file.')
 
 
 
 
 def download_file(filename, save_path):
-    file = open(os.path.join(save_path,filename),"wb")
-    file_bytes = file.read()
+    # file = open(os.path.join(save_path,filename),"wb")
+    # file_bytes = file.read()
 
     request = f"""
 GET /{filename} HTTP/1.1\r\n
 Host: {ip}\r\n
-\r\n
+<end>
 """ 
     host_response = send_request(request.encode())
 
-    headers, body = host_response.split(b'\r\n\r\n', 1)
+    headers, body = host_response.split(b'<end>', 1)
 
     if b"200 OK" in headers:
         file = open(os.path.join(save_path,filename),"wb")
